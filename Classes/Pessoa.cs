7c11866c9a50e71b3e2c82352c66de9b07d73741
juliomaciel7 <juliomacieldeveloper@outlook.com>
2022-05-21 23:08:55 -0300

    Added class

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using UC9_Senai_EncodingBackEnd_SA2.Interfaces;
+
+namespace UC9_Senai_EncodingBackEnd_SA2.Classes
+{
+    public abstract class Pessoa : IPessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public Endereco Endereco { get; set; }
+        public decimal Remuneracao { get; set; }
+
+        public abstract decimal PagarImposto(decimal remuneracao);
+    }
+}
