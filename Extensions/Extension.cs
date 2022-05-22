ceafee1ad08422f391c4b6b7e3e380fc8a9214bc
juliomaciel7 <juliomacieldeveloper@outlook.com>
2022-05-21 23:09:01 -0300

    Added class

---
+++ Extensions/Extension.cs
@@ -0,0 +1,23 @@
+﻿namespace UC9_Senai_EncodingBackEnd_SA2.Extensions
+{
+    public static class Extension
+    {
+        // Método de extensão para customizar o WriteLine
+        public static void WriteLine(this string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        // Método de extensão para customizar o Write
+        public static void Write(this string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+     }
+  }
+
