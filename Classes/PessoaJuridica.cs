eabec82a665c89cc59a1931b24f97f0f974d4f43
juliomaciel7 <juliomacieldeveloper@outlook.com>
2022-05-23 19:51:30 -0300

    cnpj method and tax implemented

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,4 +1,6 @@
-﻿using UC9_Senai_EncodingBackEnd_SA2.Interfaces;
+﻿using System.Text.RegularExpressions;
+using UC9_Senai_EncodingBackEnd_SA2.Extensions;
+using UC9_Senai_EncodingBackEnd_SA2.Interfaces;
 
 namespace UC9_Senai_EncodingBackEnd_SA2.Classes
 {
@@ -10,12 +12,59 @@ namespace UC9_Senai_EncodingBackEnd_SA2.Classes
 
         public override decimal PagarImposto(decimal remuneracao)
         {
-            throw new NotImplementedException();
+            if (remuneracao <= 1500)
+            {
+                return remuneracao * 0.03m; // Retorna 3%
+            }
+            else if (remuneracao <= 3500)
+            {
+                return remuneracao * 0.05m; // Retorna 5%
+            }
+            else if (remuneracao <= 6000)
+            {
+                return remuneracao * 0.07m; // Retorna 7%
+            }
+            else
+            {
+                return remuneracao * 0.09m; // Retorna 9%
+            }
         }
 
+        
+
+        // Análise correpondências e conferi o número total de caracteres
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            if (Regex.IsMatch(cnpj, @"(^(\d2)\.\d{3}\.\d{3}\/\d{4}\-\d{2})|(\d{18})$") || Regex.IsMatch(cnpj, @"(^(\d2)\d{3}\d{3}\/\d{4}\d{2})|(\d{16})$") || Regex.IsMatch(cnpj, @"(^(\d2)\d{3}\d{3}\d{4}\d{2})|(\d{14})$"))
+            {
+                if (cnpj.Length == 18)
+                {
+                    if (cnpj.Substring(11, 4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+                else if (cnpj.Length == 16)
+                {
+                    if (cnpj.Substring(9, 4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+                else if (cnpj.Length == 14)
+                {
+                    if (cnpj.Substring(8, 4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                "Digite seu CNPJ corretamente. Padrão: NN.NNN.NNN/NNNN-NN".WriteLine(ConsoleColor.DarkRed);
+            }
+            return false;
         }
     }
 }
