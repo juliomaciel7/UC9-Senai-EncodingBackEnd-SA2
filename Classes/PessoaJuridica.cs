15b8dc82cb0d93263afda33a8be412086d632ac8
juliomaciel7 <juliomacieldeveloper@outlook.com>
2022-05-27 12:35:15 -0300

    Updated Validate cnpj method

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -31,40 +31,26 @@ namespace UC9_Senai_EncodingBackEnd_SA2.Classes
             }
         }
 
-        
+
 
         // Análise correpondências e conferi o número total de caracteres
+
         public bool ValidarCnpj(string cnpj)
         {
-            if (Regex.IsMatch(cnpj, @"(^(\d2)\.\d{3}\.\d{3}\/\d{4}\-\d{2})|(\d{18})$") || Regex.IsMatch(cnpj, @"(^(\d2)\d{3}\d{3}\/\d{4}\d{2})|(\d{16})$") || Regex.IsMatch(cnpj, @"(^(\d2)\d{3}\d{3}\d{4}\d{2})|(\d{14})$"))
+            if (Regex.IsMatch(cnpj, @"(^(\d2).\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$"))
             {
-                if (cnpj.Length == 18)
-                {
                     if (cnpj.Substring(11, 4) == "0001")
                     {
                         return true;
                     }
-                }
-                else if (cnpj.Length == 16)
-                {
-                    if (cnpj.Substring(9, 4) == "0001")
-                    {
-                        return true;
-                    }
-                }
-                else if (cnpj.Length == 14)
-                {
-                    if (cnpj.Substring(8, 4) == "0001")
-                    {
-                        return true;
-                    }
-                }
-                return true;
+                    else
+                    "CNPJ digitado incorretamente".WriteLine(ConsoleColor.DarkRed);
             }
             else
             {
-                "Digite seu CNPJ corretamente. Padrão: NN.NNN.NNN/NNNN-NN".WriteLine(ConsoleColor.DarkRed);
+                "Digite o CNPJ corretamente. Padrão: NN.NNN.NNN/0001-NN".WriteLine(ConsoleColor.DarkRed);
             }
+
             return false;
         }
 
