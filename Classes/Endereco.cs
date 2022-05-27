5f9fe91d859acab94753fd4d00fe9c4ae3a46350
juliomaciel7 <juliomacieldeveloper@outlook.com>
2022-05-27 17:31:04 -0300

    Updated

--- Classes/Endereco.cs
+++ Classes/Endereco.cs
@@ -2,16 +2,11 @@
 
 namespace UC9_Senai_EncodingBackEnd_SA2.Classes
 {
-    public class Endereco : IEndereco
+    public class Endereco 
     {
         public string NomeRua { get; set; }
         public int NumeroCasa { get; set; }
         public string Complemento { get; set; }
         public bool Comercial { get; set; }
-
-        public bool ValidarEndereco(string endereco)
-        {
-            return false;
-        }
     }
 }
